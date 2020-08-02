4a1d5edac7f8ad57cdc7d37ca911c5293b63339c
Jia <pochialu@gmail.com>
2020-08-02 11:55:55 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()   // 取出 todo models 所有資料
     .lean()   // 將 Mongoose 的 Model 物件轉換成乾淨的 JS 資料陣列
+    .sort({ name: 'asc' }) // 'desc'
     .then(todos => res.render('index', { todos }))    // 資料傳給 index 模板
     .catch(error => console.log(error))   // 錯誤回報
 })
