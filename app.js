ee89973ded85ec4b003cd9066bb27258429a2a8f
Jia <pochialu@gmail.com>
2020-08-03 00:53:25 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
 // set database connection
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -22,11 +23,12 @@ app.engine('hbs', exphbs({ defaultLayouts: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Todo.find()   // 取出 todo models 所有資料
     .lean()   // 將 Mongoose 的 Model 物件轉換成乾淨的 JS 資料陣列
-    .sort({ name: 'asc' }) // 'desc'
+    .sort({ name: 'asc' }) // 反序：'desc'
     .then(todos => res.render('index', { todos }))    // 資料傳給 index 模板
     .catch(error => console.log(error))   // 錯誤回報
 })
@@ -58,7 +60,7 @@ app.get('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id/', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -71,7 +73,7 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 // delete
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id/', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
